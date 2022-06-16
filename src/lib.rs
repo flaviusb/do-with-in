3384c5237f57eabc1b6feaf9949ac102e83689d0
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2022-06-16 19:30:34 +1200

    Stub of do_with_in_explicit_internal! for unquote-splice attempt.

--- src/lib.rs
+++ src/lib.rs
@@ -52,4 +52,33 @@ pub fn do_with_in(t: TokenStream) -> TokenStream {
 }
 
 
+#[proc_macro]
+pub fn do_with_in_explicit_internal(t: TokenStream) -> TokenStream {
+  let t_new: TokenStream2 = t.into();
+  quote! {
+    do_with_in_explicit(c, v, #t_new).into()
+  }.into()
+}
 
+/*
+
+make output tokenstream2
+Make HashMap ToTokens if K, V are ToTokens
+Make fn thing ToTokens through... ways?
+add them to output as c=..., v=...
+add ⌜quote!{do_with_in_explicit_internal!(#ident)}
+
+let out = TokenStream2::new();
+let kv_tok   = k.to_tokens();
+let conf_tok = c.to_tokens();
+out.extend(quote!{
+  {
+    let k = #kv_tok;
+    let c = #conf_tok; 
+    do_with_in_explicit_internal!(#ident)
+  }
+});
+out
+
+
+ */
