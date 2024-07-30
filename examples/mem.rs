3a438cbaeb6ed89de36dcd438f1f57975d31e379
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2024-07-31 00:30:57 +1200

    Start work on getting the correct first chunks and shifting them for the mem example.

--- examples/mem.rs
+++ examples/mem.rs
@@ -36,7 +36,10 @@ do_with_in!{
           let safe_ip: usize = ((ip * $MEM_SIMULATING_TYPE_SIZE) % ($MEM_STORE_TYPE_SIZE * $MEM_STORE_CHUNKS)) / $MEM_SIMULATING_TYPE_SIZE;
           $(mk bit1 // $1 is current output chunk number, $2 is the final output chunk number
                     // ip is based on MEM_SIMULATING_TYPE_SIZE, and goes out to MEM_SIMULATING_CHUNKS, wrapping around at UNDERLYING_MEM_SIZE
-                   0,
+                    // To work out which chunk the first bit is in, we do ((ip + $1) * $MEM_SIMULATING_TYPE_SIZE / $MEM_STORE_TYPE_SIZE) as usize
+                    // To work out how far through the first chunk the first bit is, we do ((ip + $1) * $MEM_SIMULATING_TYPE_SIZE % $MEM_STORE_TYPE_SIZE)
+                    // To work out how many chunks it occupies, we do ((((ip + $1) * $MEM_SIMULATING_TYPE_SIZE / $MEM_STORE_TYPE_SIZE) + (($MEM_SIMULATING_TYPE_SIZE - 1) / $MEM_STORE_TYPE_SIZE)) as usize) - (((ip + $1) * $MEM_SIMULATING_TYPE_SIZE / $MEM_STORE_TYPE_SIZE) as usize)
+                   ((self.mem[((ip + $1) * $MEM_SIMULATING_TYPE_SIZE) / $MEM_STORE_TYPE_SIZE]) >> (((ip + $1) * $MEM_SIMULATING_TYPE_SIZE) % $MEM_STORE_TYPE_SIZE)) as $MEM_RETURN_TYPE,
             )
           $(mk bit2 // $1 is current output chunk number, $2 is the final output chunk number
                     // ip is based on MEM_SIMULATING_TYPE_SIZE, and goes out to MEM_SIMULATING_CHUNKS, wrapping around at UNDERLYING_MEM_SIZE
@@ -57,18 +60,42 @@ do_with_in!{
   $(mkFetch usize 64 256 u8 8 1 4 1 fetch_4_1)
   $(mkFetch usize 64 256 u8 8 1 4 2 fetch_4_2)
   $(mkFetch usize 64 256 u8 8 2 4 3 fetch_4_3)
+  $(mkFetch usize 64 256 u8 8 2 9 1 fetch_9_1)
+  $(mkFetch usize 64 256 u8 8 3 9 2 fetch_9_2)
+  $(mkFetch usize 64 256 u8 8 4 9 3 fetch_9_3)
 
 }
 
 pub fn main() {
   let mut m: Mem<usize, 256> = Mem { mem: [0usize; 256], };
-  m.mem[0] = 3;
+  m.mem[0] = 0b1010_1111_1010_0101_1100_1010_0011_1111;
   m.mem[1] = 999;
   m.mem[2] = (1 << 63) - 1;
   m.mem[3] = (1 << 63) - 1;
-  println!("ip = {}, fetching {}: {:?}", 0, 1, fetch_4_1(&m, 0));
-  println!("ip = {}, fetching {}: {:?}", 0, 2, fetch_4_2(&m, 0));
-  println!("ip = {}, fetching {}: {:?}", 0, 3, fetch_4_3(&m, 0));
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 1, "u4", fetch_4_1(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 2, "u4", fetch_4_2(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 3, "u4", fetch_4_3(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 1, "u4", fetch_4_1(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 2, "u4", fetch_4_2(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 3, "u4", fetch_4_3(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 1, "u4", fetch_4_1(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 2, "u4", fetch_4_2(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 3, "u4", fetch_4_3(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 1, "u4", fetch_4_1(&m, 42), "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 2, "u4", fetch_4_2(&m, 42), "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 3, "u4", fetch_4_3(&m, 42), "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 1, "u9", fetch_9_1(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 2, "u9", fetch_9_2(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  0, 3, "u9", fetch_9_3(&m, 0),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 1, "u9", fetch_9_1(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 2, "u9", fetch_9_2(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  1, 3, "u9", fetch_9_3(&m, 1),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 1, "u9", fetch_9_1(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 2, "u9", fetch_9_2(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}",  2, 3, "u9", fetch_9_3(&m, 2),  "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 1, "u9", fetch_9_1(&m, 42), "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 2, "u9", fetch_9_2(&m, 42), "u8");
+  println!("ip = {}, fetching {}{}: {:?}{}", 42, 3, "u9", fetch_9_3(&m, 42), "u8");
 }
 
 
